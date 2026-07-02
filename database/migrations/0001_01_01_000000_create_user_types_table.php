5678bc4ce5f966da7c51a44b3f02c877238c2d7d
EduardoJCentenoL <centenolopezeduaardojose@gmail.com>
2026-07-01 21:08:38 -0600

    fix(auth): Correccion en el nombre del campo descripcion del tipo de usuario.

--- database/migrations/0001_01_01_000000_create_user_types_table.php
+++ database/migrations/0001_01_01_000000_create_user_types_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('user_types', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name', 50);
-            $table->text('ut_description');
+            $table->text('user_type_description');
             $table->timestamps();
         });
     }
